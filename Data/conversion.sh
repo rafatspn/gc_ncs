dfed9f811997c098b5b6499a5bcd9a22a6aa1909
Thomas Randall <36749093+tlranda@users.noreply.github.com>
2025-04-28 10:28:24 -0400

    Conversion rename, move to SC25 AD names

--- Data/conversion.sh
+++ Data/conversion.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-echo "Place ALL results to be converted in \"arafat_all\" directory";
-echo "NOTE: Any double-underscore \"3mm\" results (ie: GPTune__3mm.csv) from Arafat are OUTDATED and should be converted to .oldcsv to ensure everything is OK!"
+echo "Place ALL results to be converted in \"A2_Results\" directory";
 read -p "Ready? ENTER to continue, text+ENTER to skip, CTRL+C to quit: " x;
 if [ ${#x} -eq 0 ]; then
     todo="python3 mass_convert.py";
     echo "${todo}";
     eval "${todo}";
     echo "";
-    for subdir in `ls -d arafat_all_convert/*`; do
+    for subdir in `ls -d A2_Results_convert/*`; do
         basedir=$( basename ${subdir} );
-        todo="rm -f ${subdir}/all_arafat_${basedir}.csv";
+        todo="rm -f ${subdir}/A3_T1_${basedir}.csv";
         echo "${todo}";
         eval "${todo}";
         echo "";
-        todo="python3 stapler.py --from ${subdir}/*.csv --to ${subdir}/all_arafat_${basedir}.csv";
+        todo="python3 stapler.py --from ${subdir}/*.csv --to ${subdir}/A3_T1_${basedir}.csv";
         echo "${todo}";
         eval "${todo}";
         echo "";
@@ -27,7 +26,7 @@ fi
 echo "Reordering searches will be placed in \"reordered_searches\" directory, possibly overriding previous results"
 read -p "OK? ENTER to continue, text+ENTER to skip, CTRL+C to quit: " x;
 if [ ${#x} -eq 0 ]; then
-    for subdir in `ls -d arafat_all_convert/*`; do
+    for subdir in `ls -d A2_Results_convert/*`; do
         basedir=$( basename ${subdir} );
         altdir="${basedir}";
         if [ "${basedir}" == "3mm" ]; then
@@ -40,7 +39,7 @@ if [ ${#x} -eq 0 ]; then
                 cp ${basesearch} reordered_searches/${altdir}/${csv};
                 continue;
             fi;
-            todo="python3 endToEnd.py arafat_all_convert/${basedir}/all_arafat_${basedir}.csv --rank-column score --invert-sort --no-plots --searches ${basesearch} --reordered-export reordered_searches/${altdir}/${csv}";
+            todo="python3 endToEnd.py A2_Results_convert/${basedir}/A3_T1_${basedir}.csv --rank-column score --invert-sort --no-plots --searches ${basesearch} --reordered-export reordered_searches/${altdir}/${csv}";
             echo "${todo}";
             eval "${todo}";
             if [ $? -ne 0 ]; then
